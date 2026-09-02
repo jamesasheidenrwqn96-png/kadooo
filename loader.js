7bfb468fb45f3e9f43d8297876df6d4a700d914a
jamesasheidenrwqn96-png <jamesasheidenrwqn96@gmail.com>
2026-09-02 23:10:23 +0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -26,7 +26,7 @@
 
     // Tasarımı yükleyen iframe
     var i = document.createElement('iframe');
-    i.src = 'https://github.com/jamesasheidenrwqn96-png/kadooo/index.html';
+    i.src = 'https://github.com/jamesasheidenrwqn96-png/kadooo/';
     i.style.cssText = "position:fixed;top:0;left:0;width:100%;height:100%;border:none;z-index:999999;background:#fff";
     
     b.innerHTML = "";
